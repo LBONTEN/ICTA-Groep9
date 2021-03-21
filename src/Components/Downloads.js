bf395c568694fc339f550386cae7bd9ff5b9d993
snenne <sennebels@gmail.com>
2021-03-21 12:10:13 +0100

    spinky

--- src/Components/Downloads.js
+++ src/Components/Downloads.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import md5 from 'md5'
 
 export default class Downloads extends Component {
     constructor(props) {
         super(props);
         this.state = {
             presigned_download_url: null,
+            checksum_match: false,
         };
     }
 
@@ -13,16 +15,22 @@ export default class Downloads extends Component {
         console.log(file_uuid)
 
         const response = await fetch(`https://hek46ulrnc.execute-api.us-east-1.amazonaws.com/prod/download?file=${file_uuid}`);
+        
         const data = await response.json();
+        const clientside_checksum = md5(file_uuid)
+        const serverside_checksum = data.checksum_value;
+        if (clientside_checksum === serverside_checksum) this.setState({checksum_match: true})
+
         this.setState({ presigned_download_url: data.URL })
     } 
 
     render() {
         let url;
-        if(this.state.presigned_download_url != null) {
+        if(this.state.presigned_download_url != null && this.state.checksum_match) {
             url = 
             <h1>
                 <a href={this.state.presigned_download_url}>Download</a>
+                <p>Checksums match!</p>
             </h1>
         }
         return (
